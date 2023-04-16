e8b9e4db06c9a823e4fe5cd1fc396f0c9d4c1045
Cubxity <contact@cubxity.dev>
2023-04-16 15:48:39 +0200

    feat(editor): bibtex syntax highlighting

--- src/lib/editor/monaco.ts
+++ src/lib/editor/monaco.ts
@@ -3,9 +3,12 @@ import * as oniguruma from "vscode-oniguruma";
 import onigurumaWasm from "vscode-oniguruma/release/onig.wasm?url";
 import { Registry } from "vscode-textmate";
 
-import grammar from "./data/grammar.json";
-import theme from "./data/theme.json";
 import { wireTextMateGrammars } from "./grammar";
+import bibtex from "./lang/bibtex.json";
+import typstTm from "./lang/typst-tm.json";
+import theme from "./theme/theme.json";
+
+type IMonarchLanguage = monaco.languages.IMonarchLanguage;
 
 export const initMonaco = (async () => {
   // Don't use streaming due to MIME type mismatch.
@@ -14,10 +17,11 @@ export const initMonaco = (async () => {
   const wasm = await fetch(onigurumaWasm).then((res) => res.arrayBuffer());
   await oniguruma.loadWASM(wasm);
 
+  // Register TextMate grammars
   const registry = new Registry({
     onigLib: Promise.resolve(oniguruma),
     loadGrammar() {
-      return Promise.resolve(grammar);
+      return Promise.resolve(typstTm);
     },
   });
 
@@ -26,6 +30,10 @@ export const initMonaco = (async () => {
 
   monaco.languages.register({ id: "typst", extensions: ["typ"] });
   await wireTextMateGrammars(registry, { typst: "source.typst" });
+
+  // Register Monarch languages
+  monaco.languages.register({ id: "bibtex", extensions: ["bib"] });
+  monaco.languages.setMonarchTokensProvider("bibtex", bibtex as IMonarchLanguage);
 
   monaco.editor.defineTheme("dracula", theme as monaco.editor.IStandaloneThemeData);
   monaco.editor.setTheme("dracula");
