48e1f64d30040d1b9620b2db2f6e78119375cc78
Cubxity <contact@cubxity.dev>
2023-04-16 12:05:28 +0200

    feat(preview): lazy rendering

--- src/components/PreviewPage.svelte
+++ src/components/PreviewPage.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { TypstRenderResponse } from "../lib/ipc";
   import { render } from "../lib/ipc";
+  import { onMount } from "svelte";
 
   export let page: number;
   export let hash: string;
@@ -9,15 +10,33 @@
   export let scale: number;
 
   let canvas: HTMLCanvasElement;
+  let canRender = false;
+  let isIntersecting = false;
 
-  const update = async (hash: string, updateScale: number) => {
+  onMount(() => {
+    const observer = new IntersectionObserver((entries) => {
+      // Don't update canRender when the preview page is no longer visible
+      isIntersecting = entries[0].isIntersecting;
+      if (isIntersecting) canRender = true;
+    });
+    observer.observe(canvas);
+    return () => observer.disconnect();
+  });
+
+  const invalidateCanRender = (_hash: string, _scale: number) => {
+    canRender = isIntersecting;
+  };
+
+  const update = async (updateHash: string, updateScale: number) => {
     // return
     const res: TypstRenderResponse = await render(page, updateScale);
 
     const img = new Image(width, height);
     img.src = "data:image/png;base64," + res.image;
     img.onload = () => {
-      if (scale === updateScale) {
+      // Prevent out-of-order rendering
+      // TODO: Cancel pending renders? Accept in-order renders?
+      if (hash === updateHash && scale === updateScale) {
         const ctx = canvas.getContext("2d");
         canvas.width = width;
         canvas.height = height;
@@ -27,7 +46,8 @@
     };
   };
 
-  $: update(hash, scale);
+  $: invalidateCanRender(hash, scale);
+  $: if (canRender) update(hash, scale);
 </script>
 
 <div
