f606d3bb7cc14cb666dd803f03d201f4cb0dc656
Caoimhe <caoimhebyrne06@gmail.com>
2023-10-29 23:55:10 +0000

    fix(fs): declare file exclusions as a static array

--- src-tauri/src/ipc/commands/fs.rs
+++ src-tauri/src/ipc/commands/fs.rs
@@ -25,6 +25,9 @@ pub enum FileType {
     Directory,
 }
 
+/// Files to be excluded from directory listings.
+const EXCLUDED_FILES: &[&str] = &[".DS_Store", ".localized"];
+
 /// Reads raw bytes from a specified path.
 /// Note that this command is slow compared to the text API due to Wry's
 /// messaging system in v1. See: https://github.com/tauri-apps/tauri/issues/1817
@@ -125,8 +128,7 @@ pub async fn fs_list_dir<R: Runtime>(
 
                 // These files are irrelevant to most text editing applications, and most of them
                 // filter these out too.
-                let should_ignore = name == ".DS_Store" || name == ".localized";
-                if !should_ignore {
+                if !EXCLUDED_FILES.contains(&name.as_str()) {
                     files.push(FileItem { name, file_type: t });
                 }
             }
